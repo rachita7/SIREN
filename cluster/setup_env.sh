cb66f0399c2a1431ab317e89a8f228e32dc8cb1f
Rachita Agrawal <rachitaagrawal@student-net-hix-0393.intern.ethz.ch>
2026-07-13 13:42:00 +0200

    Miniconda set-up and directory update

--- cluster/setup_env.sh
+++ cluster/setup_env.sh
@@ -9,17 +9,36 @@
 
 set -e
 
-# ETH Euler: login nodes have internet, no eth_proxy needed here. Conda is not
-# a module on Euler; install miniconda into your $HOME or scratch first if you
-# don't have it (https://docs.conda.io/en/latest/miniconda.html).
-# On other clusters, load your conda/cuda modules here, e.g.:
+# ETH Euler: login nodes have internet (no eth_proxy needed here). Conda is not
+# a module on Euler; install Miniconda ONCE into $HOME before running this script:
+#
+#   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#   bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+#   $HOME/miniconda3/bin/conda init bash && source ~/.bashrc
+#   conda config --set auto_activate_base false
+#   # keep downloaded packages off $HOME's file quota:
+#   conda config --add pkgs_dirs /cluster/scratch/$USER/conda_pkgs
+#
+# On other clusters, load your conda/cuda modules here instead, e.g.:
 # module load miniconda3
 # module load cuda/12.6
 
-# Keep the (large) HF cache on scratch/project storage, not in $HOME.
-# TODO: point this at your scratch space.
-export HF_HOME="${HF_HOME:-$HOME/scratch/hf_cache}"
+if ! command -v conda >/dev/null 2>&1; then
+    echo "ERROR: conda not found. Install Miniconda first (see comments above)." >&2
+    exit 1
+fi
+
+# Keep the (large) HF cache off $HOME. On Euler /cluster/scratch/$USER is used
+# automatically. NOTE: Euler scratch is purged after ~15 days of file age; if
+# jobs later fail with missing weights, just re-run cluster/download_assets.py.
+if [ -d "/cluster/scratch/$USER" ]; then
+    export HF_HOME="${HF_HOME:-/cluster/scratch/$USER/hf_cache}"
+else
+    export HF_HOME="${HF_HOME:-$HOME/hf_cache}"
+fi
 mkdir -p "$HF_HOME"
+# Keep pip's cache off $HOME too (used during conda env create's pip stage)
+export PIP_CACHE_DIR="${PIP_CACHE_DIR:-$HF_HOME/../pip_cache}"
 
 REPO_URL="https://github.com/rachita7/SIREN.git"
 REPO_DIR="${REPO_DIR:-$HOME/SIREN}"
